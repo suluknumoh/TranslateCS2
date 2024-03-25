bf9ceb410c51339b1315186e870590c603ea9b76
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-03-25 17:23:01 +0100

    refactoring

--- TranslateCS2/Helpers/LittleEndianBase128.cs
+++ TranslateCS2/Helpers/LittleEndianBase128.cs
@@ -4,6 +4,8 @@ using System.IO;
 namespace TranslateCS2.Helpers;
 /// <seealso href="https://en.wikipedia.org/wiki/LEB128"/>
 internal static class LittleEndianBase128 {
+
+    private static readonly int _shiftCountSevenBits = 7;
     private static readonly byte _highOrderBitBitMask = 0x80;
     private static readonly byte _lowOrderBitsBitMask = 0x7f;
 
@@ -14,8 +16,8 @@ internal static class LittleEndianBase128 {
         byte b;
         do {
             b = (byte) stream.ReadByte();
-            result |= (uint) (b & 0x7f) << shift;
-            shift += 7;
+            result |= (uint) (b & _lowOrderBitsBitMask) << shift;
+            shift += _shiftCountSevenBits;
         } while ((b & _highOrderBitBitMask) != 0);
         return result;
     }
@@ -24,20 +26,19 @@ internal static class LittleEndianBase128 {
     public static byte[] EncodeUnsignedInteger(uint value) {
         List<byte> bytes = [];
         uint work = value;
-        while (true) {
+        byte b;
+        do {
             // 0x7f is 01111111 in bits
             // to get low-order 7 bits of work-value
-            byte b = (byte) (work & _lowOrderBitsBitMask);
-            work >>= 7;
+            b = (byte) (work & _lowOrderBitsBitMask);
+            work >>= _shiftCountSevenBits;
             if (work != 0) {
                 // 0x80 is 10000000 in bits
                 // to set high-order bit of work-value
                 b |= _highOrderBitBitMask;
-                bytes.Add(b);
-            } else {
-                bytes.Add(b);
-                return bytes.ToArray();
             }
-        }
+            bytes.Add(b);
+        } while (work != 0);
+        return bytes.ToArray();
     }
 }
