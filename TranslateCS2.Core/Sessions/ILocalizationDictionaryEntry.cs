dfc2db7d1cea0014715c56f6cf88a715e85cb173
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-08 22:51:09 +0200

    JsonIgnore for Interface

--- TranslateCS2.Core/Sessions/ILocalizationDictionaryEntry.cs
+++ TranslateCS2.Core/Sessions/ILocalizationDictionaryEntry.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 using TranslateCS2.Core.Helpers;
 
 namespace TranslateCS2.Core.Sessions;
 public interface ILocalizationDictionaryEntry {
+    [JsonIgnore]
     List<string> Keys { get; }
     string Key => this.Keys[0];
+    [JsonIgnore]
     int Count => this.Keys.Count;
+    [JsonIgnore]
     string Value { get; }
+    [JsonIgnore]
     string ValueLanguageCode { get; }
+    [JsonIgnore]
     string? ValueMerge { get; set; }
+    [JsonIgnore]
     string? ValueMergeLanguageCode { get; set; }
     string? Translation { get; set; }
+    [JsonIgnore]
     bool IsTranslated => !StringHelper.IsNullOrWhiteSpaceOrEmpty(this.Translation);
     void AddKey(string key) {
         if (key == null) {
