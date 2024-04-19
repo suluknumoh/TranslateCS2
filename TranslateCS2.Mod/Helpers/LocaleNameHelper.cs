ef01c5e67555ca261c28db94a4fc11421246e962
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-19 15:52:12 +0200

    a LocaleNameHelper

---
+++ TranslateCS2.Mod/Helpers/LocaleNameHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Game.SceneFlow;
+
+namespace TranslateCS2.Mod.Helpers;
+internal static class LocaleNameHelper {
+    public static bool IsLocaleNameAvailable(string localeName) {
+        string[] alreadySupportedLocales = GameManager.instance.localizationManager.GetSupportedLocales();
+        foreach (string alreadySupportedLocale in alreadySupportedLocales) {
+            string alreadySupportedLocaleName = GameManager.instance.localizationManager.GetLocalizedName(alreadySupportedLocale);
+            if (localeName.Equals(alreadySupportedLocaleName, StringComparison.OrdinalIgnoreCase)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
