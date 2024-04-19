1093598fb5dbc6c3f43a61e1b539ba6269390e28
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-19 15:51:59 +0200

    a TranslationFile

---
+++ TranslateCS2.Mod/Models/TranslationFile.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text;
+
+namespace TranslateCS2.Mod.Models;
+internal class TranslationFile {
+    public string Name { get; }
+    public string Path { get; }
+    public TranslationFile(string name, string path) {
+        this.Name = name;
+        this.Path = path;
+    }
+    public override string ToString() {
+        return $"{nameof(this.Name)}: {this.Name} - {nameof(this.Path)}: {this.Path}";
+    }
+    public string ReadJson() {
+        return File.ReadAllText(this.Path, Encoding.UTF8);
+    }
+}
