9a32df14fd5edf4d08df0c36e63d4f1cd8eacb37
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-19 15:50:46 +0200

    format

--- TranslateCS2/Controls/Ribbons/AppRibbonControl.xaml.cs
+++ TranslateCS2/Controls/Ribbons/AppRibbonControl.xaml.cs
@@ -1,8 +1,10 @@
-﻿namespace TranslateCS2.Controls.Ribbons;
+﻿using System.Windows.Controls.Ribbon;
+
+namespace TranslateCS2.Controls.Ribbons;
 /// <summary>
 /// Interaktionslogik für AppRibbonControl.xaml
 /// </summary>
-public partial class AppRibbonControl : System.Windows.Controls.Ribbon.Ribbon {
+public partial class AppRibbonControl : Ribbon {
     public AppRibbonControl() {
         this.InitializeComponent();
     }
