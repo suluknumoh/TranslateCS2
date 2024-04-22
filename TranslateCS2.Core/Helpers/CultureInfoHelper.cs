3b700878318b05155cd317a1164186b8177b167a
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-22 15:42:20 +0200

    CultureInfoHelper

---
+++ TranslateCS2.Core/Helpers/CultureInfoHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace TranslateCS2.Core.Helpers;
+public static class CultureInfoHelper {
+    public static CultureInfo? GatherCultureFromEnglishName(string? englishName) {
+        if (englishName == null) {
+            return null;
+        }
+        CultureInfo[] specificCultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+        IEnumerable<CultureInfo> guessedCultures = specificCultures.Where(item => item.EnglishName.StartsWith(englishName));
+        IOrderedEnumerable<CultureInfo> orderedGuessedCultures = guessedCultures.OrderBy(item => item.Name);
+        if (orderedGuessedCultures.Any()) {
+            return orderedGuessedCultures.First();
+        }
+        return null;
+    }
+}
