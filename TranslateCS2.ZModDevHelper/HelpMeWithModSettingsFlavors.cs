36f05016b03ecbb1923d021d8a4895461b0839de
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-27 12:31:01 +0200

    helper to generate code blocks etc

---
+++ TranslateCS2.ZModDevHelper/HelpMeWithModSettingsFlavors.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+using TranslateCS2.ModBridge;
+
+namespace TranslateCS2.ZModDevHelper;
+public class HelpMeWithModSettingsFlavors {
+    /// <summary>
+    ///     generates the codeblocks within the partial class ModSettingsFlavors
+    /// </summary>
+    [Fact(Skip = "dont help me each time")]
+    public void GenerateCodeBlocks() {
+        IEnumerable<UnitySystemLanguage> systemLanguages = Enum.GetValues(typeof(UnitySystemLanguage)).OfType<UnitySystemLanguage>();
+        StringBuilder builder = new StringBuilder();
+        foreach (UnitySystemLanguage systemLanguage in systemLanguages) {
+            if (systemLanguage is UnitySystemLanguage.Unknown or UnitySystemLanguage.Chinese) {
+                continue;
+            }
+            builder.AppendLine($"private string _Flavor{systemLanguage} = InitFlavor(SystemLanguage.{systemLanguage});");
+            builder.AppendLine($"[SettingsUIDropdown(typeof(ModSettings), nameof(GetFlavors{systemLanguage}))]");
+            builder.AppendLine($"[SettingsUISection(Section, FlavorGroup)]");
+            builder.AppendLine($"[SettingsUIHideByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Hidden))]");
+            builder.AppendLine($"[SettingsUIDisableByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Disabled))]");
+            builder.AppendLine($"public string Flavor{systemLanguage} {{");
+            builder.AppendLine($"    get => this._Flavor{systemLanguage};");
+            builder.AppendLine($"    set => this._Flavor{systemLanguage} = this.GetValueToSet(SystemLanguage.{systemLanguage}, value);");
+            builder.AppendLine($"}}");
+            builder.AppendLine($"public DropdownItem<string>[] GetFlavors{systemLanguage}() {{");
+            builder.AppendLine($"    return GetFlavors(SystemLanguage.{systemLanguage});");
+            builder.AppendLine($"}}");
+            builder.AppendLine($"public bool IsFlavor{systemLanguage}Hidden() {{");
+            builder.AppendLine($"    return IsHidden(SystemLanguage.{systemLanguage});");
+            builder.AppendLine($"}}");
+            builder.AppendLine($"public bool IsFlavor{systemLanguage}Disabled() {{");
+            builder.AppendLine($"    return IsDisabled(SystemLanguage.{systemLanguage});");
+            builder.AppendLine($"}}");
+            builder.AppendLine();
+            builder.AppendLine();
+            builder.AppendLine();
+        }
+        string text = builder.ToString();
+        Assert.True(true);
+    }
+    [Fact(Skip = "dont help me each time")]
+    public void GenerateLables() {
+        IEnumerable<UnitySystemLanguage> systemLanguages = Enum.GetValues(typeof(UnitySystemLanguage)).OfType<UnitySystemLanguage>();
+        StringBuilder builder = new StringBuilder();
+        foreach (UnitySystemLanguage systemLanguage in systemLanguages) {
+            if (systemLanguage is UnitySystemLanguage.Unknown or UnitySystemLanguage.Chinese) {
+                continue;
+            }
+            builder.AppendLine($"{{ this.modSettings.GetOptionLabelLocaleID(nameof(ModSettings.Flavor{systemLanguage})), this.GetLabel(nameof(ModSettings.Flavor{systemLanguage}))}},");
+        }
+        string text = builder.ToString();
+        Assert.True(true);
+    }
+    [Fact(Skip = "dont help me each time")]
+    public void GenerateDescriptions() {
+        IEnumerable<UnitySystemLanguage> systemLanguages = Enum.GetValues(typeof(UnitySystemLanguage)).OfType<UnitySystemLanguage>();
+        StringBuilder builder = new StringBuilder();
+        foreach (UnitySystemLanguage systemLanguage in systemLanguages) {
+            if (systemLanguage is UnitySystemLanguage.Unknown or UnitySystemLanguage.Chinese) {
+                continue;
+            }
+            builder.AppendLine($"{{ this.modSettings.GetOptionDescLocaleID(nameof(ModSettings.Flavor{systemLanguage})), this.GetDescription(nameof(ModSettings.Flavor{systemLanguage}))}},");
+        }
+        string text = builder.ToString();
+        Assert.True(true);
+    }
+}
---
+++ TranslateCS2.ZModDevHelper/HelpMeWithSupportedLanguages.cs
@@ -0,0 +1,90 @@
+using System.Globalization;
+using System.Text;
+
+using TranslateCS2.Core.Helpers;
+using TranslateCS2.ModBridge;
+
+
+namespace TranslateCS2.ZModDevHelper;
+
+public class HelpMeWithSupportedLanguages {
+    private readonly Dictionary<UnitySystemLanguage, List<CultureInfo>> Dict = [];
+    [Fact(Skip = "dont help me each time")]
+    public void GenerateJsons() {
+        string directoryPath = PathHelper.TryToGetModsPath();
+        IList<string> supportedLocales = LocalesSupported.List;
+        foreach (string supportedLocale in supportedLocales) {
+            string content = $"{{ \"Options.SECTION[General]\": \"General ({supportedLocale}.json)\" }}";
+            string filePath = Path.Combine(directoryPath, $"{supportedLocale}{ModConstants.JsonExtension}");
+            if (File.Exists(filePath)) {
+                File.Delete(filePath);
+            }
+            File.WriteAllText(filePath, content, Encoding.UTF8);
+        }
+    }
+
+    /// <summary>
+    ///     generates the list of supported language/region-codes for the long description and readme markdowns
+    /// </summary>
+    [Fact(Skip = "dont help me each time")]
+    public void GenerateMarkdownList() {
+        IList<string> supportedLocales = LocalesSupported.List;
+        this.FillDictionary(supportedLocales);
+        IOrderedEnumerable<KeyValuePair<UnitySystemLanguage, List<CultureInfo>>> ordered = this.Dict.OrderBy(item => item.Key.ToString());
+        StringBuilder builder = new StringBuilder();
+        foreach (KeyValuePair<UnitySystemLanguage, List<CultureInfo>> entry in ordered) {
+            builder.AppendLine($"## {entry.Key.ToString()}");
+            foreach (CultureInfo? cultureInfo in entry.Value) {
+                builder.AppendLine($"* {cultureInfo.Name} - {CultureInfoHelper.EaseLocaleName(cultureInfo)}");
+            }
+            builder.AppendLine();
+            builder.AppendLine();
+        }
+        string supportedLanguagesMarkDown = builder.ToString().Replace("&", "and");
+        Assert.True(true);
+
+    }
+
+    private void FillDictionary(IList<string> supportedLocales) {
+        IEnumerable<UnitySystemLanguage> languages = Enum.GetValues(typeof(UnitySystemLanguage)).OfType<UnitySystemLanguage>();
+        CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
+            .Where(culture => supportedLocales.Contains(culture.Name)).ToArray();
+        foreach (CultureInfo culture in cultures) {
+            foreach (UnitySystemLanguage language in languages) {
+                string? comparator = null;
+                switch (language) {
+                    case UnitySystemLanguage.Chinese:
+                        continue;
+                    case UnitySystemLanguage.SerboCroatian:
+                        if (culture.EnglishName.StartsWith(LangConstants.Serbian, StringComparison.OrdinalIgnoreCase)
+                            || culture.EnglishName.StartsWith(LangConstants.Croatian, StringComparison.OrdinalIgnoreCase)
+                        ) {
+                            this.AddToDictionary(culture, language);
+                        }
+                        continue;
+                    case UnitySystemLanguage.ChineseSimplified:
+                        comparator = LangConstants.ChineseSimplified;
+                        break;
+                    case UnitySystemLanguage.ChineseTraditional:
+                        comparator = LangConstants.ChineseTraditional;
+                        break;
+                    default:
+                        comparator = language.ToString();
+                        break;
+                }
+                if (culture.EnglishName.StartsWith(comparator, StringComparison.OrdinalIgnoreCase)) {
+                    this.AddToDictionary(culture, language);
+                }
+            }
+        }
+    }
+
+    private void AddToDictionary(CultureInfo culture, UnitySystemLanguage language) {
+        if (this.Dict.TryGetValue(language, out List<CultureInfo>? cultureInfos) && cultureInfos != null) {
+            cultureInfos.Add(culture);
+        } else {
+            cultureInfos = [culture];
+            this.Dict.Add(language, cultureInfos);
+        }
+    }
+}
