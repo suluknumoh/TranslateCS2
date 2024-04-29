c284dcbf301152952733a16b13e402a511e90090
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-29 10:15:59 +0200

    strings

--- TranslateCS2.Mod/Helpers/DropDownItemsHelper.cs
+++ TranslateCS2.Mod/Helpers/DropDownItemsHelper.cs
@@ -4,7 +4,7 @@ using Game.UI.Widgets;
 
 namespace TranslateCS2.Mod.Helpers;
 internal static class DropDownItemsHelper {
-    public static string None { get; } = "none";
+    public static string None { get; } = nameof(None).ToLower();
     public static List<DropdownItem<string>> GetDefault(bool addNone) {
         List<DropdownItem<string>> flavors = [];
         if (addNone) {
--- TranslateCS2.ModBridge/LangConstants.cs
+++ TranslateCS2.ModBridge/LangConstants.cs
@@ -2,6 +2,6 @@
 public static class LangConstants {
     public static string ChineseSimplified { get; } = "Chinese (Simplified";
     public static string ChineseTraditional { get; } = "Chinese (Traditional";
-    public static string Croatian { get; } = "Croatian";
-    public static string Serbian { get; } = "Serbian";
+    public static string Croatian { get; } = nameof(Croatian);
+    public static string Serbian { get; } = nameof(Serbian);
 }
