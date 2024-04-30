672dd44b6e35734266c596f47d0fde1d1aebb877
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-04-30 11:01:15 +0200

    got unnecessary

--- TranslateCS2.ModBridge/RegExConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace TranslateCS2.ModBridge;
-public static class RegExConstants {
-    public static Regex IsOnlyAZCaseInSensitive { get; } = new Regex("^[a-zA-Z]+$");
-}
