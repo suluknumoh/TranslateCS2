f2edc530724689789b1cb96dbe9b04b482070663
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-05-07 18:55:35 +0200

    trying to understand autogenerated properties

--- TranslateCS2.ZModDevHelper/HelpMeWithModSettingsFlavors.cs
+++ TranslateCS2.ZModDevHelper/HelpMeWithModSettingsFlavors.cs
@@ -18,28 +18,25 @@ public class HelpMeWithModSettingsFlavors {
             if (systemLanguage is SystemLanguage.Unknown or SystemLanguage.Chinese) {
                 continue;
             }
-            builder.AppendLine($"private string _Flavor{systemLanguage} = InitFlavor(SystemLanguage.{systemLanguage});");
-            builder.AppendLine($"[Include]");
-            builder.AppendLine($"[SettingsUIDropdown(typeof(ModSettings), nameof(GetFlavors{systemLanguage}))]");
-            builder.AppendLine($"[SettingsUISection(Section, FlavorGroup)]");
-            builder.AppendLine($"[SettingsUIHideByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Hidden))]");
-            builder.AppendLine($"[SettingsUIDisableByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Disabled))]");
-            builder.AppendLine($"public string Flavor{systemLanguage} {{");
-            builder.AppendLine($"    get => this._Flavor{systemLanguage};");
-            builder.AppendLine($"    set => this._Flavor{systemLanguage} = this.GetValueToSet(SystemLanguage.{systemLanguage}, value);");
-            builder.AppendLine($"}}");
+            //builder.AppendLine($"private string _Flavor{systemLanguage} = InitFlavor(SystemLanguage.{systemLanguage});");
+            //builder.AppendLine($"[Include]");
+            //builder.AppendLine($"[SettingsUIDropdown(typeof(ModSettings), nameof(GetFlavors{systemLanguage}))]");
+            //builder.AppendLine($"[SettingsUISection(Section, FlavorGroup)]");
+            //builder.AppendLine($"[SettingsUIHideByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Hidden))]");
+            //builder.AppendLine($"[SettingsUIDisableByCondition(typeof(ModSettings), nameof(IsFlavor{systemLanguage}Disabled))]");
+            //builder.AppendLine($"public string Flavor{systemLanguage} {{");
+            //builder.AppendLine($"    get => this._Flavor{systemLanguage};");
+            //builder.AppendLine($"    set => this._Flavor{systemLanguage} = this.GetValueToSet(SystemLanguage.{systemLanguage}, value);");
+            //builder.AppendLine($"}}");
             builder.AppendLine($"public DropdownItem<string>[] GetFlavors{systemLanguage}() {{");
             builder.AppendLine($"    return GetFlavors(SystemLanguage.{systemLanguage});");
             builder.AppendLine($"}}");
-            builder.AppendLine($"public bool IsFlavor{systemLanguage}Hidden() {{");
-            builder.AppendLine($"    return IsHidden(SystemLanguage.{systemLanguage});");
-            builder.AppendLine($"}}");
-            builder.AppendLine($"public bool IsFlavor{systemLanguage}Disabled() {{");
-            builder.AppendLine($"    return IsDisabled(SystemLanguage.{systemLanguage});");
-            builder.AppendLine($"}}");
-            builder.AppendLine();
-            builder.AppendLine();
-            builder.AppendLine();
+            //builder.AppendLine($"public bool IsFlavor{systemLanguage}Hidden() {{");
+            //builder.AppendLine($"    return IsHidden(SystemLanguage.{systemLanguage});");
+            //builder.AppendLine($"}}");
+            //builder.AppendLine($"public bool IsFlavor{systemLanguage}Disabled() {{");
+            //builder.AppendLine($"    return IsDisabled(SystemLanguage.{systemLanguage});");
+            //builder.AppendLine($"}}");
         }
         string text = builder.ToString()
             //.ReplaceLineEndings("\n")
@@ -54,7 +51,7 @@ public class HelpMeWithModSettingsFlavors {
             if (systemLanguage is SystemLanguage.Unknown or SystemLanguage.Chinese) {
                 continue;
             }
-            builder.AppendLine($"this.AddToDictionary(this.modSettings.GetOptionLabelLocaleID(nameof(ModSettings.Flavor{systemLanguage})), this.GetLabel(SystemLanguage.{systemLanguage}), true);");
+            builder.AppendLine($"this.AddToDictionary(this.modSettings.GetOptionLabelLocaleID(\"Flavor{systemLanguage}\"), this.GetLabel(SystemLanguage.{systemLanguage}), true);");
         }
         string text = builder.ToString()
             //.ReplaceLineEndings("\n")
@@ -69,7 +66,7 @@ public class HelpMeWithModSettingsFlavors {
             if (systemLanguage is SystemLanguage.Unknown or SystemLanguage.Chinese) {
                 continue;
             }
-            builder.AppendLine($"this.AddToDictionary(this.modSettings.GetOptionDescLocaleID(nameof(ModSettings.Flavor{systemLanguage})), this.GetDescription(SystemLanguage.{systemLanguage}), true);");
+            builder.AppendLine($"this.AddToDictionary(this.modSettings.GetOptionDescLocaleID(\"Flavor{systemLanguage}\"), this.GetDescription(SystemLanguage.{systemLanguage}), true);");
         }
         string text = builder.ToString()
             //.ReplaceLineEndings("\n")
