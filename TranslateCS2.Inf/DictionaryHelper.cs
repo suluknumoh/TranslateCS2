a3d230e0a51477dc27a9c6bec3777147f2016085
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-05-15 07:27:54 +0200

    update

--- TranslateCS2.Inf/DictionaryHelper.cs
+++ TranslateCS2.Inf/DictionaryHelper.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace TranslateCS2.Inf;
 public static class DictionaryHelper {
-    public static IDictionary<string, string> GetNonEmpty(Dictionary<string, string> localizationDictionary) {
+    public static IDictionary<string, string> GetNonEmpty(IDictionary<string, string> localizationDictionary) {
         return
             localizationDictionary
                 .Where(item =>
