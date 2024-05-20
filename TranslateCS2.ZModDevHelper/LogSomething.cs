8ac822c838b9a39b672cf848297a06a64ec7e76a
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-05-20 13:31:37 +0200

    LogSomething examples and docs

---
+++ TranslateCS2.ZModDevHelper/LogSomething.cs
@@ -0,0 +1,12 @@
+using TranslateCS2.Inf.Loggers;
+using TranslateCS2.ZZZTestLib;
+
+namespace TranslateCS2.ZModDevHelper;
+public class LogSomething {
+    [Fact]
+    public void LogTest() {
+        IMyLogProvider logProvider = TestLogger.GetTestLogProvider<LogSomething>();
+        ModTestRuntimeContainer runtimeContainer = new ModTestRuntimeContainer(logProvider);
+        runtimeContainer.Logger.LogInfo(this.GetType(), "This is just a test:", [0]);
+    }
+}
--- TranslateCS2.ZZZTestLib/TestLogger.cs
+++ TranslateCS2.ZZZTestLib/TestLogger.cs
@@ -1,9 +1,32 @@
+using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 using TranslateCS2.Inf.Loggers;
 
 namespace TranslateCS2.ZZZTestLib;
 public static class TestLogger {
+    /// <summary>
+    ///     creates a subdirectory named "Logs"
+    ///     <br/>
+    ///     <br/>
+    ///     within that subdirectory, another subdirectory is created and named via <see cref="Type"/>s <see cref="MemberInfo.Name"/>
+    ///     <br/>
+    ///     <br/>
+    ///     and should be used per method
+    ///     <br/>
+    ///     <br/>
+    ///     cause one log-file per test-method is generated via <paramref name="callerMemberName"/>
+    /// </summary>
+    /// <typeparam name="T">
+    ///     <see cref="Type"/> of Test-<see langword="class"/>
+    /// </typeparam>
+    /// <param name="callerMemberName">
+    ///     leave this parameter
+    /// </param>
+    /// <returns>
+    ///     an <see cref="IMyLogProvider"/> to use with <see cref="IMyLogger"/>
+    /// </returns>
     public static IMyLogProvider GetTestLogProvider<T>([CallerMemberName] string callerMemberName = "") {
         Microsoft.Extensions.Logging.ILogger<T> logger = TestLoggerInternal.GetLogger<T>(callerMemberName: callerMemberName);
         return new TestLogProvider<T>(logger);
