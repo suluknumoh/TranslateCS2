cf4f94eebcecf216d8c45942394286831f777640
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-05 08:45:38 +0200

    formatting

--- TranslateCS2.Mod/Properties/I18N/I18NMod.Designer.cs
+++ TranslateCS2.Mod/Properties/I18N/I18NMod.Designer.cs
@@ -106,7 +106,15 @@ namespace TranslateCS2.Mod.Properties.I18N {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Generates a .json-file named &apos;{0}&apos; with all Key-Value-Pairs for this Mod that can be found in this mods data-directory &apos;{1}&apos; after a click on this button. The .json-file named &apos;{0}&apos; is NOT read by this Mod. It enables you to integrate the included key-value pairs into a corresponding language file and to translate the entries for this Mod..
+        ///   Looks up a localized string similar to Generates a .json-file named
+        ///&apos;{0}&apos;
+        ///with all Key-Value-Pairs for this Mod that can be found in this mods data-directory
+        ///&apos;{1}&apos;
+        ///after a click on this button.
+        ///The .json-file named
+        ///&apos;{0}&apos;
+        ///is NOT read by this Mod.
+        ///It enables you to integrate the included key-value pairs into a corresponding language file and to translate the entries for this Mod..
         /// </summary>
         internal static string GroupGenerateButtonGenerateDescription {
             get {
@@ -151,7 +159,10 @@ namespace TranslateCS2.Mod.Properties.I18N {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Reloads the language file(s) that existed at startup. Does not unload language files. Does not load new language files. Files have to be located within this directory &apos;{0}&apos;.
+        ///   Looks up a localized string similar to Reloads the language file(s) that existed at startup.
+        ///Does not unload language files.
+        ///Does not load new language files.
+        ///Files have to be located within this directory &apos;{0}&apos;.
         /// </summary>
         internal static string GroupReloadButtonReloadDescription {
             get {
