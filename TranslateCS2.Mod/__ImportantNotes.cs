0e160103624b72cdc2afb8a73126bd9e6a259dce
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-07 10:00:34 +0200

    updated important notes

--- TranslateCS2.Mod/__ImportantNotes.cs
+++ TranslateCS2.Mod/__ImportantNotes.cs
@@ -3,3 +3,6 @@
 ///
 /// Paths/Directory-Structure
 /// <see href="https://cs2.paradoxwikis.com/Naming_Folder_And_Files"/>
+///
+/// Setting-Options
+/// <see href="https://cs2.paradoxwikis.com/Options_UI"/>
