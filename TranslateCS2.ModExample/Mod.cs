4cef688b314e6f20abb9272a5292db3931656b0d
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-07 09:56:24 +0200

    ModExample

---
+++ TranslateCS2.ModExample/Mod.cs
@@ -0,0 +1,9 @@
+using Game;
+using Game.Modding;
+
+namespace TranslateCS2.ModExample {
+    public class Mod : IMod {
+        public void OnLoad(UpdateSystem updateSystem) { }
+        public void OnDispose() { }
+    }
+}
---
+++ TranslateCS2.ModExample/README.md
@@ -0,0 +1,2 @@
+# Example Mod
+This is an example, how to distribute one or more unofficial locales that are read by 'TranslateCS2'
