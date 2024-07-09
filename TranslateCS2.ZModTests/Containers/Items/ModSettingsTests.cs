570403e18758c1c736491b40318329e5a5f7606d
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-09 11:40:48 +0200

    more tests

--- TranslateCS2.ZModTests/Containers/Items/ModSettingsTests.cs
+++ TranslateCS2.ZModTests/Containers/Items/ModSettingsTests.cs
@@ -70,7 +70,6 @@ public class ModSettingsTests : AProvidesTestDataOk {
             Assert.Equal(expectedPreviousBuiltIn, modSettings.PreviousLocale);
             expectedValueVersion++;
             Assert.Equal(expectedValueVersion, modSettings.GetValueVersion());
-
         }
     }
 
@@ -103,6 +102,54 @@ public class ModSettingsTests : AProvidesTestDataOk {
                 Assert.Equal(language.Value.Flavors.First().Id, setted.Value);
             }
             Assert.Single(locManagerProvider.Sources[language.Value.Id]);
+        }
+    }
+
+    [Fact]
+    public void HandleLocaleOnUnLoadTest() {
+        IEnumerable<SystemLanguage> systemLanguages = Enum.GetValues(typeof(SystemLanguage)).OfType<SystemLanguage>();
+        ITestLogProvider testLogProvider = TestLogProviderFactory.GetTestLogProvider<ModSettingsTests>();
+        foreach (SystemLanguage systemLanguage in systemLanguages) {
+            if (SystemLanguage.Chinese == systemLanguage) {
+                continue;
+            }
+            ModTestRuntimeContainer runtimeContainer = ModTestRuntimeContainer.Create(testLogProvider,
+                                                                                      userDataPath: this.dataProvider.DirectoryName);
+            runtimeContainer.Init();
+            runtimeContainer.TestLocManagerProvider.AddBuiltIn();
+            ModSettings modSettings = runtimeContainer.Settings;
+            string expectedPrevious = runtimeContainer.LocManager.FallbackLocaleId;
+            MyLanguages languages = runtimeContainer.Languages;
+            MyLanguage? language = languages.GetLanguage(systemLanguage);
+            Assert.NotNull(language);
+            IntSettings interfaceSettings = runtimeContainer.IntSettings;
+            // apply
+            // has to be set 'manually'
+            // at game-runtime, it works the other way around
+            // a user selects a Language within the default interface settings
+            // and ModSettings.Apply(Setting setting) is called
+            interfaceSettings.CurrentLocale = language.Id;
+            modSettings.ApplyNewLocale(language.Id);
+            Assert.Equal(language.Name, modSettings.CurrentLanguage);
+            if (language.IsBuiltIn) {
+                expectedPrevious = language.Id;
+            }
+            // is expected before and after dispose
+            Assert.Equal(expectedPrevious, modSettings.PreviousLocale);
+            // dispose with new apllied
+            runtimeContainer.Dispose();
+            // is expected before and after dispose
+            Assert.Equal(expectedPrevious, modSettings.PreviousLocale);
+            // this mods locale has to be the selected languages id
+            Assert.Equal(language.Id, modSettings.Locale);
+            if (language.IsBuiltIn) {
+                // in general,
+                // this assertion does not make sense,
+                // cause interfaceSettings.CurrentLocale is set manually a few lines above
+                Assert.Equal(language.Id, interfaceSettings.CurrentLocale);
+            } else {
+                Assert.Equal(expectedPrevious, interfaceSettings.CurrentLocale);
+            }
         }
     }
 }
