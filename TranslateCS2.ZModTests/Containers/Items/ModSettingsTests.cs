e214282035b41e2d5e6b4b1d7e1ea43ef97b62f6
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-09 09:17:33 +0200

    more tests

--- TranslateCS2.ZModTests/Containers/Items/ModSettingsTests.cs
+++ TranslateCS2.ZModTests/Containers/Items/ModSettingsTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using TranslateCS2.Inf;
 using TranslateCS2.Mod.Containers.Items;
 using TranslateCS2.Mod.Helpers;
 using TranslateCS2.ZModTests.TestHelpers.Containers;
@@ -16,6 +17,62 @@ using Xunit;
 namespace TranslateCS2.ZModTests.Containers.Items;
 public class ModSettingsTests : AProvidesTestDataOk {
     public ModSettingsTests(TestDataProvider testDataProvider) : base(testDataProvider) { }
+
+    [Fact]
+    public void SectionNameTest() {
+        Assert.Equal("Main", ModSettings.Section);
+    }
+
+    [Fact]
+    public void SetDefaultsTest() {
+        ITestLogProvider testLogProvider = TestLogProviderFactory.GetTestLogProvider<ModSettingsTests>();
+        ModTestRuntimeContainer runtimeContainer = ModTestRuntimeContainer.Create(testLogProvider,
+                                                                                  userDataPath: this.dataProvider.DirectoryName);
+        runtimeContainer.Init();
+        ModSettings modSettings = runtimeContainer.Settings;
+        Assert.Equal(StringConstants.All, modSettings.ExportDropDown);
+        string expectedDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        Assert.Equal(expectedDirectory, modSettings.DefaultDirectory);
+        Assert.Equal(expectedDirectory, modSettings.GenerateDirectory);
+        Assert.Equal(expectedDirectory, modSettings.ExportDirectory);
+        Assert.True(modSettings.LoadFromOtherMods);
+    }
+
+    [Fact]
+    public void ApplyTest() {
+        // Apply(Setting setting);
+        // cannot be tested
+    }
+
+    [Fact]
+    public void ApplyNewLocaleTest() {
+        ITestLogProvider testLogProvider = TestLogProviderFactory.GetTestLogProvider<ModSettingsTests>();
+        ModTestRuntimeContainer runtimeContainer = ModTestRuntimeContainer.Create(testLogProvider,
+                                                                                  userDataPath: this.dataProvider.DirectoryName);
+        runtimeContainer.Init();
+        runtimeContainer.TestLocManagerProvider.AddBuiltIn();
+        ModSettings modSettings = runtimeContainer.Settings;
+        Dictionary<SystemLanguage, MyLanguage> languageDictionary = runtimeContainer.Languages.LanguageDictionary;
+        string expectedPreviousBuiltIn = runtimeContainer.LocManager.FallbackLocaleId;
+        int expectedValueVersion = 0;
+        foreach (KeyValuePair<SystemLanguage, MyLanguage> entry in languageDictionary) {
+            SystemLanguage systemLanguage = entry.Key;
+            MyLanguage language = entry.Value;
+            modSettings.ApplyNewLocale(language.Id);
+            Assert.Equal(language.Name, modSettings.CurrentLanguage);
+            string settedFlavor = modSettings.FlavorsSetted[systemLanguage];
+            if (language.IsBuiltIn) {
+                Assert.Equal(DropDownItemsHelper.None, settedFlavor);
+                expectedPreviousBuiltIn = language.Id;
+            } else {
+                Assert.NotEqual(DropDownItemsHelper.None, settedFlavor);
+            }
+            Assert.Equal(expectedPreviousBuiltIn, modSettings.PreviousLocale);
+            expectedValueVersion++;
+            Assert.Equal(expectedValueVersion, modSettings.GetValueVersion());
+
+        }
+    }
 
     [Fact]
     public void HandleLocaleOnLoadTest() {
