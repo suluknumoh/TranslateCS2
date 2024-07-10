a5e03860ea4822044832ad1b4a378257942f95f8
suluknumoh <130506604+suluknumoh@users.noreply.github.com>
2024-07-10 09:29:23 +0200

    only fallback need export entries, cause only fallback can be exported

--- TranslateCS2.Mod/Containers/Items/ModSettingsLocaleSource.cs
+++ TranslateCS2.Mod/Containers/Items/ModSettingsLocaleSource.cs
@@ -13,18 +13,18 @@ internal class ModSettingsLocaleSource : IDictionarySource {
     public IReadOnlyDictionary<string, string> AllEntries => this._AllEntries;
     private readonly Dictionary<string, string> _ExportableEntries = [];
     public IReadOnlyDictionary<string, string> ExportableEntries => this._ExportableEntries;
+    public string LocaleId { get; }
+    public bool IsFallBack { get; }
     public ModSettingsLocaleSource(ModSettings modSettings,
-                                   string locale,
-                                   bool isFallBackLocale) {
-        this.Init(modSettings,
-                  locale,
-                  isFallBackLocale);
+                                   string localeId,
+                                   bool isFallBack) {
+        this.LocaleId = localeId;
+        this.IsFallBack = isFallBack;
+        this.Init(modSettings);
     }
 
-    private void Init(ModSettings modSettings,
-                      string locale,
-                      bool isFallBackLocale) {
-        string path = $"{typeof(I18NMod).Namespace}{StringConstants.Dot}{locale}{ModConstants.JsonExtension}";
+    private void Init(ModSettings modSettings) {
+        string path = $"{typeof(I18NMod).Namespace}{StringConstants.Dot}{this.LocaleId}{ModConstants.JsonExtension}";
         IDictionary<string, string>? dictionary = JsonHelper.DeSerializeFromAssembly<Dictionary<string, string>>(path);
         if (dictionary is null) {
             return;
@@ -33,7 +33,7 @@ internal class ModSettingsLocaleSource : IDictionarySource {
         if (dictionary.Count == 0) {
             return;
         }
-        if (isFallBackLocale) {
+        if (this.IsFallBack) {
             this.AddToDictionary(modSettings.GetSettingsLocaleID(),
                                  ModConstants.NameSimple,
                                  false);
@@ -51,110 +51,82 @@ internal class ModSettingsLocaleSource : IDictionarySource {
 
     private void AddSettingsGroup(ModSettings modSettings, IDictionary<string, string> dictionary) {
         this.AddToDictionary(modSettings.GetOptionGroupLocaleID(ModSettings.SettingsGroup),
-                             dictionary[I18NMod.GroupSettingsTitle],
-                             true);
+                             dictionary[I18NMod.GroupSettingsTitle]);
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.LoadFromOtherMods)),
-                             dictionary[I18NMod.GroupSettingsToggleLoadFromOtherModsLabel],
-                             true);
+                             dictionary[I18NMod.GroupSettingsToggleLoadFromOtherModsLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.LoadFromOtherMods)),
-                             dictionary[I18NMod.GroupSettingsToggleLoadFromOtherModsDescription],
-                             true);
+                             dictionary[I18NMod.GroupSettingsToggleLoadFromOtherModsDescription]);
     }
 
     private void AddReloadGroup(ModSettings modSettings, IDictionary<string, string> dictionary) {
         this.AddToDictionary(modSettings.GetOptionGroupLocaleID(ModSettings.ReloadGroup),
-                             dictionary[I18NMod.GroupReloadTitle],
-                             true);
+                             dictionary[I18NMod.GroupReloadTitle]);
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.ReloadLanguages)),
-                             dictionary[I18NMod.GroupReloadButtonReloadLabel],
-                             true);
+                             dictionary[I18NMod.GroupReloadButtonReloadLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.ReloadLanguages)),
-                             dictionary[I18NMod.GroupReloadButtonReloadDescription],
-                             true);
+                             dictionary[I18NMod.GroupReloadButtonReloadDescription]);
         this.AddToDictionary(modSettings.GetOptionWarningLocaleID(nameof(ModSettings.ReloadLanguages)),
-                             dictionary[I18NMod.GroupReloadButtonReloadConfirmation],
-                             true);
+                             dictionary[I18NMod.GroupReloadButtonReloadConfirmation]);
     }
 
     private void AddGenerateGroup(ModSettings modSettings, IDictionary<string, string> dictionary) {
         this.AddToDictionary(modSettings.GetOptionGroupLocaleID(ModSettings.GenerateGroup),
-                             dictionary[I18NMod.GroupGenerateTitle],
-                             true);
+                             dictionary[I18NMod.GroupGenerateTitle]);
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.LogMarkdownAndCultureInfoNames)),
-                             dictionary[I18NMod.GroupGenerateButtonLogMarkdownAndCultureInfoNamesLabel],
-                             true);
+                             dictionary[I18NMod.GroupGenerateButtonLogMarkdownAndCultureInfoNamesLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.LogMarkdownAndCultureInfoNames)),
-                             dictionary[I18NMod.GroupGenerateButtonLogMarkdownAndCultureInfoNamesDescription],
-                             true);
+                             dictionary[I18NMod.GroupGenerateButtonLogMarkdownAndCultureInfoNamesDescription]);
         //
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.GenerateDirectory)),
-                             dictionary[I18NMod.GroupGenerateGenerateDirectoryLabel],
-                             true);
+                             dictionary[I18NMod.GroupGenerateGenerateDirectoryLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.GenerateDirectory)),
                              this.Format(dictionary[I18NMod.GroupGenerateGenerateDirectoryDescription],
-                                               ModConstants.ModExportKeyValueJsonName),
-                             true);
+                                               ModConstants.ModExportKeyValueJsonName));
         //
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.GenerateLocalizationJson)),
-                             dictionary[I18NMod.GroupGenerateButtonGenerateLabel],
-                             true);
+                             dictionary[I18NMod.GroupGenerateButtonGenerateLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.GenerateLocalizationJson)),
                              this.Format(dictionary[I18NMod.GroupGenerateButtonGenerateDescription],
-                                               ModConstants.ModExportKeyValueJsonName),
-                             true);
+                                               ModConstants.ModExportKeyValueJsonName));
         this.AddToDictionary(modSettings.GetOptionWarningLocaleID(nameof(ModSettings.GenerateLocalizationJson)),
-                             dictionary[I18NMod.GroupGenerateButtonGenerateConfirmation],
-                             true);
+                             dictionary[I18NMod.GroupGenerateButtonGenerateConfirmation]);
     }
 
     private void AddFlavorGroup(ModSettings modSettings, IDictionary<string, string> dictionary) {
         this.AddToDictionary(modSettings.GetOptionGroupLocaleID(ModSettings.FlavorGroup),
-                             dictionary[I18NMod.GroupFlavorTitle],
-                             true);
+                             dictionary[I18NMod.GroupFlavorTitle]);
 
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.CurrentLanguage)),
-                             dictionary[I18NMod.GroupFlavorCurrentLanguageLabel],
-                             true);
+                             dictionary[I18NMod.GroupFlavorCurrentLanguageLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.CurrentLanguage)),
-                             dictionary[I18NMod.GroupFlavorCurrentLanguageDescription],
-                             true);
+                             dictionary[I18NMod.GroupFlavorCurrentLanguageDescription]);
 
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.FlavorDropDown)),
-                             dictionary[I18NMod.GroupFlavorFlavorLabel],
-                             true);
+                             dictionary[I18NMod.GroupFlavorFlavorLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.FlavorDropDown)),
-                             dictionary[I18NMod.GroupFlavorFlavorDescription],
-                             true);
+                             dictionary[I18NMod.GroupFlavorFlavorDescription]);
     }
 
     private void AddExportGoup(ModSettings modSettings, IDictionary<string, string> dictionary) {
         this.AddToDictionary(modSettings.GetOptionGroupLocaleID(ModSettings.ExportGroup),
-                             dictionary[I18NMod.GroupExportTitle],
-                             true);
+                             dictionary[I18NMod.GroupExportTitle]);
 
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.ExportDropDown)),
-                             dictionary[I18NMod.GroupExportExportDropDownLabel],
-                             true);
+                             dictionary[I18NMod.GroupExportExportDropDownLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.ExportDropDown)),
-                             dictionary[I18NMod.GroupExportExportDropDownDescription],
-                             true);
+                             dictionary[I18NMod.GroupExportExportDropDownDescription]);
 
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.ExportDirectory)),
-                             dictionary[I18NMod.GroupExportExportDirectoryLabel],
-                             true);
+                             dictionary[I18NMod.GroupExportExportDirectoryLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.ExportDirectory)),
-                             dictionary[I18NMod.GroupExportExportDirectoryDescription],
-                             true);
+                             dictionary[I18NMod.GroupExportExportDirectoryDescription]);
 
         this.AddToDictionary(modSettings.GetOptionLabelLocaleID(nameof(ModSettings.ExportButton)),
-                             dictionary[I18NMod.GroupExportExportButtonLabel],
-                             true);
+                             dictionary[I18NMod.GroupExportExportButtonLabel]);
         this.AddToDictionary(modSettings.GetOptionDescLocaleID(nameof(ModSettings.ExportButton)),
-                             dictionary[I18NMod.GroupExportExportButtonDescription],
-                             true);
+                             dictionary[I18NMod.GroupExportExportButtonDescription]);
         this.AddToDictionary(modSettings.GetOptionWarningLocaleID(nameof(ModSettings.ExportButton)),
-                             dictionary[I18NMod.GroupExportExportButtonConfirmation],
-                             true);
+                             dictionary[I18NMod.GroupExportExportButtonConfirmation]);
     }
 
     private string? Format(string? format, string? content) {
@@ -163,6 +135,13 @@ internal class ModSettingsLocaleSource : IDictionarySource {
             return null;
         }
         return String.Format(format, content);
+    }
+
+    private void AddToDictionary(string key,
+                                 string? value) {
+        this.AddToDictionary(key,
+                             value,
+                             this.IsFallBack);
     }
 
     private void AddToDictionary(string key,
